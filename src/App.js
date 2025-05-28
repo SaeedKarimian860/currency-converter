5bc1f55338b3d0cb1723b3b500aebfc531b50dfc
SaeedKarimian860 <SGK12190@gmail.com>
2025-05-28 19:23:44 -0400

    currency converter

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ export default function App() {
   const [fromCur, setFromCur] = useState("EUR");
   const [toCur, setToCur] = useState("USD");
   const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function convert() {
+        setIsLoading(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
         );
         const data = await res.json();
         setConverted(data.rates[toCur]);
+        setIsLoading(false);
       }
 
+      if (fromCur === toCur) return setConverted(amount);
       convert();
     },
     [amount, fromCur, toCur]
@@ -27,14 +31,23 @@ export default function App() {
         type="text"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
       />
-      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
