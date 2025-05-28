5185803ea512e27284c92cda70c213d210eb4fe4
SaeedKarimian860 <SGK12190@gmail.com>
2025-05-28 18:44:44 -0400

    currency converter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [amount, setAmount] = useState(1);
   const [fromCur, setFromCur] = useState("EUR");
   const [toCur, setToCur] = useState("USD");
+
+  useEffect(function () {
+    async function convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+
+    convert();
+  }, [amount, fromCur, toCur]);
 
   return (
     <div>
@@ -12,13 +24,13 @@ export default function App() {
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
       />
-      <select>
+      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
